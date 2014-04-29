c53438ad8de1d6d162157520046cf33dae7981af
Austin W Ellis <austinellis+git@gmail.com>
2014-04-29 15:30:57 -0700

    add setup.py

---
+++ setup.py
@@ -0,0 +1,22 @@
+import os
+import platform
+from setuptools import setup
+
+
+
+setup(
+      name='pymonster',
+      version='0.1',
+      url='https://github.com/texuf/pymonster',
+      classifiers = [
+          'Programming Language :: Python :: 2.7',
+          ],
+      description='An event logging and distributed event consuming library.',
+      license='MIT',
+      author='Austin Ellis',
+      author_email='austinellis@gmail.com',
+      py_modules=[],
+      install_requires=[],
+      scripts=[],
+      namespace_packages=[]
+      )
